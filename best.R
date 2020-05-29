9c4d844dd7ee52e1303d89a58723b4021c6521cf
iskren89 <blagov.iskren@gmail.com>
2020-05-29 11:42:45 +0300

    added error msg

--- best.R
+++ best.R
@@ -4,11 +4,15 @@ hosp[, 11] <- as.numeric(hosp[, 11])
 hosp[, 17] <- as.numeric(hosp[, 17])
 hosp[, 23] <- as.numeric(hosp[, 23])
 outcomes <- c("heart attack"=11, "heart failure"=17, "pneumonia"=23)
+if (!(state %in% hosp$State)) {result <- "Invalid State"}
+else if (!outcome %in% names(outcomes)) {result <- "Invalid Outcome"}
+else{
 my_data <- hosp[, c(2,7,outcomes[outcome])]
 names(my_data)<-c("hospital","state","outcome")
 try <- split(my_data,my_data$state)
 what <- as.data.frame(try[state])
 best <- what[order(what[3],na.last=NA,decreasing=FALSE),]
-return(best[1,1])
+result <- (best[1,1])}
+return(result)
 }
 
